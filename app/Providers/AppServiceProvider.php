7a6c6c9246280ae3781ba00812a92053db1de091
ramadanriz <zerorider96@gmail.com>
2023-05-04 07:53:30 +0700

    fix authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Gate::define('admin', function(User $user) {
-            return $user->username === 'admin';
+            return $user->is_admin;
         });
     }
 }
